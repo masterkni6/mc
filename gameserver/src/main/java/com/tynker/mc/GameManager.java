69aa9864826c9a6df621334903f7615ff6b71b20
Den <calphacygni@gamil.com>
2016-05-13 16:21:20 -0700

    added abstric functions for onplayerjoin

--- gameserver/src/main/java/com/tynker/mc/GameManager.java
+++ gameserver/src/main/java/com/tynker/mc/GameManager.java
@@ -3,14 +3,18 @@ package com.tynker.mc;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.Listener;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.block.Block;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class GameManager extends JavaPlugin implements Listener{
    
     private int gameDimension;
-    private List<>;
+    private int playersNum;
+    private List<Player> playerlist = new ArrayList<Player>();
 
  
     @Override
@@ -23,9 +27,23 @@ public class GameManager extends JavaPlugin implements Listener{
         getServer().getScheduler().cancelTasks(this);
     }
     
-    
+    public void launchGame(List<Player> players, int logNumber){
+
+    }   
+
+   public int getFreeLot(){
+
+	return 0;
+    }
+ 
     @EventHandler
     public void PlayerJoin(PlayerJoinEvent event) {
+	playerlist.add(event.getPlayer());
+	if(playerlist.size() == playersNum){
+		launchGame(playerlist, getFreeLot());
+	}
+
+
     }
     
 }
