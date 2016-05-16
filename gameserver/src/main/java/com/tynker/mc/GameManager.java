acd1ba6d7f61e9765e11f2d3ae2a3aa13bf1c923
Den <calphacygni@gamil.com>
2016-05-16 10:32:48 -0700

    added interval to check players number

--- gameserver/src/main/java/com/tynker/mc/GameManager.java
+++ gameserver/src/main/java/com/tynker/mc/GameManager.java
@@ -5,6 +5,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.Listener;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.lang.Integer;
 import org.bukkit.block.Block;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,9 +15,8 @@ public class GameManager extends JavaPlugin implements Listener{
    
     private int gameDimension;
     private int playersNum;
-    private List<Player> playerlist = new ArrayList<Player>();
-    private List<int> lotlist = new ArrayList<int>();
-
+    private List<Player> playerList = new ArrayList<Player>();
+    private List<List<Player>> lotList = new ArrayList<List<Player>>();
  
     @Override
     public void onEnable() {
@@ -32,19 +32,27 @@ public class GameManager extends JavaPlugin implements Listener{
 
     }   
 
-   public int getFreeLot(){
+    public int getFreeLot(){
 	
 	return 0;
     }
- 
+
+    Bukkit.getScheduler().runTaskTimer(this, new Runnable(){
+        @Override
+        public void run(){
+            if(playerList.size() > playersNum-1){
+			    int lotNum = getFreeLot();
+			    lotList.add(lotNum, playerList);
+			    launchGame(playerList.subList(0,playersNum-1), lotNum);
+	            playerList = new Array;
+	        }
+        }
+    }, 0l, (long)(1 * 1000 / 50)); 
+
     @EventHandler
     public void PlayerJoin(PlayerJoinEvent event) {
-	playerlist.add(event.getPlayer());
-	if(playerlist.size() == playersNum){
-		launchGame(playerlist, getFreeLot());
-	}
-
-
+	    synchronized(this){
+		    playerList.add(event.getPlayer());
+        }   
     }
-    
 }
