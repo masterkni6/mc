e55d00f43ea40260dcdf865f7f7243b500483197
Duc <masterkni666@gmail.com>
2016-05-16 10:35:00 -0700

    implemented getFreeLot

--- gameserver/src/main/java/com/tynker/mc/GameManager.java
+++ gameserver/src/main/java/com/tynker/mc/GameManager.java
@@ -13,38 +13,40 @@ import java.util.List;
 public class GameManager extends JavaPlugin implements Listener{
    
     private int gameDimension;
-    private int playersNum;
-    private List<Player> playerlist = new ArrayList<Player>();
-    private List<int> lotlist = new ArrayList<int>();
+    private int playersPerLot;
+    private List<Player> playerList = new ArrayList<Player>();
+    private List<List<Player>> lotList = new ArrayList<List<Player>>();
 
- 
+
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
     }
-    
+
     @Override
     public void onDisable() {
         getServer().getScheduler().cancelTasks(this);
     }
-    
-    public void launchGame(List<Player> players, int lotNumber){
 
+    //to be inherited by real games
+    public void launchGame(List<Player> players, int lotNumber){
     }   
 
-   public int getFreeLot(){
-	
-	return 0;
+    public int getFreeLot(){
+        List<Player> playerList;
+        for(int i = 0; i < lotList.size(); i++){
+            playerList = lotList[i];
+            if(!playerList.size()){
+                return i;
+            } 
+        }
+        lotList.add(new ArrayList<Player>);
+        return lotList.size()-1;
     }
- 
+
     @EventHandler
     public void PlayerJoin(PlayerJoinEvent event) {
-	playerlist.add(event.getPlayer());
-	if(playerlist.size() == playersNum){
-		launchGame(playerlist, getFreeLot());
-	}
-
-
+        playerList.add(event.getPlayer());
     }
     
 }
